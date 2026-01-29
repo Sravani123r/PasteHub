5cec47f917e9965b37abdbb1344da662c2336949
Sravani123r <bsreddy3020@gmail.com>
2026-01-29 23:01:44 +0530

    Pastebin

--- server/src/main/java/com/pastebin/pastebin/controller/PasteController.java
+++ server/src/main/java/com/pastebin/pastebin/controller/PasteController.java
@@ -12,7 +12,6 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/pastes")
-@CrossOrigin(origins = "http://localhost:5173")
 public class PasteController {
 
     private final PasteService pasteService;
